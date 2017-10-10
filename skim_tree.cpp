b2f366ce032eb607d7f5283fb6b0c4e222058e12
Axel Schmidt <schmidta@mit.edu>
2017-10-10 18:35:03 -0400

    Added Mariana's electron PID data file.

--- skim_tree.cpp
+++ skim_tree.cpp
@@ -3,16 +3,36 @@
 
 #include "TFile.h"
 #include "TTree.h"
+#include "TH2.h"
+#include "TF1.h"
 
 using namespace std;
 
+// Range of momentum for which we have good fits for PID
+const double min_el_mom = 1.5; // in GeV
+const double max_el_mom = 3.8;
+const double epratio_sig_cutrange=3.; // +/- sigma for PID cut
+
+// Target position definition
+const double min_Z = -15.; // Extremely wide settings for now. 
+const double max_Z = 15.; 
+
 int main(int argc, char ** argv)
 {
-  if (argc < 2)
+  if (argc != 3)
     {
-      cerr << "Not enough arguments. Give me a root file I can open.\n";
+      cerr << "Wrong number of arguments. Instead try\n"
+	   << "\tskim_tree /path/to/input/file /path/to/output/file\n";
       return -1;	
     }
+
+  // We're going to need to read in a bunch of root files with important calibration data
+  char filenameBuffer[256];
+  strcpy(filenameBuffer,getenv("HOME"));
+  strcat(filenameBuffer,"/.e2a/el_Epratio_mom4461.root");
+  TFile * cal_file=new TFile(filenameBuffer);
+  TF1 *el_Ep_ratio_mean=(TF1*)cal_file->Get("f_mean");
+  TF1 *el_Ep_ratio_sig=(TF1*)cal_file->Get("f_sig");    
 
   // Open up the Root file
   TFile * f = new TFile(argv[1]);
@@ -48,7 +68,7 @@ int main(int argc, char ** argv)
   t->SetBranchAddress("EC_in",EC_in); // Inner layer of ECal for each particle
   t->SetBranchAddress("EC_out",EC_out); // Outer layer of ECal for each particle
   t->SetBranchAddress("EC_tot",EC_tot); // Total energy deposit in the ECal for each particle
-  t->SetBranchAddress("Nphe",Nphe); // Number of photons per particle???
+  t->SetBranchAddress("Nphe",Nphe); // Number of photo-electrons per hit in the Cherenkov detectors
   t->SetBranchAddress("SC_Time",SC_Time); // Time in the scintillators per particle
   t->SetBranchAddress("SC_Path",SC_Path); // Path Length per particle
   t->SetBranchAddress("Charge",charge); // Charge per particle
@@ -63,27 +83,98 @@ int main(int argc, char ** argv)
   t->SetBranchAddress("TargetZ",targetZ); // Target Z per particle
   t->SetBranchAddress("Thetapq",theta_pq); // Angle wrt to q vector per particle
   t->SetBranchAddress("BjorkenX",&Xb); // Bjorken X
-  t->SetBranchAddress("STT",&STT); // STT
+  t->SetBranchAddress("STT",&STT); // RF-corrected start time.
   t->SetBranchAddress("Q2",&Q2); // Momentum transfer
   t->SetBranchAddress("W",&W); // Hadronic mass
   t->SetBranchAddress("Nu",&Nu); // Energy transfer
-  t->SetBranchAddress("Yb",&Yb); // Y-scaling variable ???
-  
+  t->SetBranchAddress("Yb",&Yb); // Y-scaling variable
+
+  // Open up the output file
+  TFile * outfile = new TFile(argv[2],"RECREATE");
+  TH2D * hist_e_thetaMom = new TH2D("e_thetaMom","e- passing fid. cuts;Theta [deg];Mom [GeV];Counts",40,10.,50.,60,0.,6.);
+  TH2D * hist_e_xQ2 = new TH2D("e_xQ2","e- passing fid. cuts;x;Q2 [GeV^2];Counts",40,0.,2.,40,0.,10.);
+  TTree * outtree = new TTree("T","Skimmed tree");
+  double e_vz, e_mom[3];
+  int nProtons;
+  outtree->Branch("e_vz",&e_vz,"e_vz/D");
+  outtree->Branch("e_mom",e_mom,"e_mom[3]/D");
+  outtree->Branch("nProtons",&nProtons,"nProtons/I");
+
   // Loop over events
   for (int event=0; event < nEvents ; event++)
     {
-      t->GetEvent(event);
-      
-      cout << "Reading event " << event << " which has " << gPart << " particles\n";
-      cout << "\tElectron candidate: " << mom[0] << " GeV   " << theta[0] << " deg.   Q^2 = " << Q2 << " GeV^2 \n";      
-      // Loop over the particles in the event
-      for (int particle=0 ; particle < gPart ; particle++)
+      if (event % 100000 == 0)
 	{
-	  cout << "\t\tParticle " << particle << " had type " << id_guess[particle] 
-	       << "   " << mom[particle] << " GeV    " << theta[particle] << " deg\n";
+	  cerr << "Working on event " << event << " out of " << nEvents << "\n";
 	}
+
+      t->GetEvent(event);
+
+      if (gPart <= 0) continue; // Ignore events that have no particle candidates
+
+
+      // ************************************************************************
+      // Here's where we do electron fiducial cuts
+
+      // Define the electron candidate energy in the EC
+      double el_cand_EC = TMath::Max(EC_in[0] + EC_out[0], EC_tot[0]);
+      
+      // We enforce bounds on acceptable E/p for electron candidates. 
+      bool el_cand_goodMom = ((mom[0] < max_el_mom) && (mom[0] > min_el_mom));
+      double min_E_over_p = (el_cand_goodMom) ? ( el_Ep_ratio_mean->Eval(mom[0]) - epratio_sig_cutrange*el_Ep_ratio_sig->Eval(mom[0])) :
+	(mom[0] > max_el_mom)? ( el_Ep_ratio_mean->Eval(max_el_mom) - epratio_sig_cutrange*el_Ep_ratio_sig->Eval(max_el_mom)) : 0.;
+      double max_E_over_p = (el_cand_goodMom) ? ( el_Ep_ratio_mean->Eval(mom[0]) + epratio_sig_cutrange*el_Ep_ratio_sig->Eval(mom[0])) :
+	(mom[0] > max_el_mom)? ( el_Ep_ratio_mean->Eval(max_el_mom) + epratio_sig_cutrange*el_Ep_ratio_sig->Eval(max_el_mom)) : 0.;	
+
+      // Decide if the electron passes fiducial cuts
+      if (!( (gPart > 0) &&  // at least one particle in the event
+	   (StatEC[0] > 0) && // EC status is good for the electron candidate
+	   (StatCC[0] > 0) && // CC status is good for the electron candidate
+	   (StatSC[0] > 0) && // SC status is good for the electron candidate
+	   (charge[0] < 0) && // Electron candidate curvature direction is negative
+	   (EC_in[0] > 0.055) && // Electron candidate has enough energy deposit in inner layer of EC
+	   (el_cand_EC > 0.33) && // Enough total energy in the EC
+	   (el_cand_EC/mom[0] > min_E_over_p ) && // E/p is in suitable range
+	   (el_cand_EC/mom[0] < max_E_over_p ) &&
+	   (mom[0] > min_el_mom) && // Electron candidate has enough momentum to be within fiducial PID region
+	   (targetZ[0] > min_Z) && // Vertex is within the target region
+	   (targetZ[0] < max_Z)
+	     ))
+	{
+	  continue;
+	}
+
+      // If we get to here, then the electron passed fiducial cuts
+      // Fill some diagnostic histograms
+      hist_e_thetaMom->Fill(theta[0],mom[0]);
+      hist_e_xQ2->Fill(Xb,Q2);
+
+      // Loop over events looking for protons
+      for (int i=1 ; i<gPart ; i++)
+	{
+	  // Still to implement
+	  cerr << "Looking to see if particle " << i << " is a proton.\n";
+	}
+
+      // Prep the output tree
+      e_vz=targetZ[0];
+      e_mom[0] = px[0];
+      e_mom[1] = py[0];
+      e_mom[2] = pz[0];
+      nProtons=0;
+
+      // Fill the output tree
+      outtree->Fill();
     }
 
+  // Clean up
   f->Close();
+  cal_file->Close();
+
+  // Write the output file
+  outtree->Write();
+  hist_e_thetaMom->Write();
+  hist_e_xQ2->Write();
+  outfile->Close();
   return 0;
 }

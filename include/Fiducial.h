00be9f52a1d5dea40e7f409a79f5b85b30dc1bb0
Reynier Cruz Torres <reynier@mit.edu>
2017-11-17 11:11:45 -0500

    Implemented "read_p_fid_params" which reads parameters for hadron fiducial cuts

--- include/Fiducial.h
+++ include/Fiducial.h
@@ -16,10 +16,10 @@ class Fiducial
 		bool inFidRegion(TVector3 mom, int charge);
 		bool in_e_EoverP(double EoverP, double mom, double cut_sigma);
 		bool in_p_deltaT(double delta_t, double mom, double cut_sigma);
+		bool   pFiducialCut(TVector3 momentum);
+		bool   CutUVW(TVector3 ecxyz);
 		TVector3 eMomentumCorrection(TVector3 V3el);
 		TVector3 FindUVW(TVector3 xyz);
-		Bool_t   pFiducialCut(TVector3 momentum);
-		Bool_t   CutUVW(TVector3 ecxyz);
 	private:
 		int E1;
 		int torus_current;
@@ -27,11 +27,12 @@ class Fiducial
 		std::string homedir;
 
 		// Helper functions
-		bool read_e_fid_params();
+		bool read_e_fid_params ();
 		bool read_e_pcor_params();
-		bool read_e_pid_params();
-		bool read_p_pid_params();
-		bool read_vz_cor_params();
+		bool read_e_pid_params ();
+		bool read_p_fid_params ();
+		bool read_p_pid_params ();
+		bool read_vz_cor_params();	
 
 		void getElectronPhiLimits(double mom, double theta, int sector, double &phiMin, double &phiMax);
 
@@ -44,6 +45,18 @@ class Fiducial
 		// Momentum Correction Data
 		double fgPar_Phi[6][3];
 		double fgPar_Theta[6][4];
+
+		// Hadron Correction Data
+		double fgPar_Pfidft1l[6][6];
+		double fgPar_Pfidft1r[6][6];
+		double fgPar_Pfidft2l[6][6];
+		double fgPar_Pfidft2r[6][6];
+		double fgPar_Pfidbt1l[6][6];
+		double fgPar_Pfidbt1r[6][6];
+		double fgPar_Pfidbt2l[6][6];
+		double fgPar_Pfidbt2r[6][6];
+		double fgPar_Pfidbl  [6][6];
+		double fgPar_Pfidbr  [6][6];
 
 		// Vertex z correction data
 		TF1 *vz_corr_func;		
